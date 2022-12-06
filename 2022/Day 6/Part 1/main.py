ea66576510d94c82feb7ff8792cc49b0b7ef1a0a
tel0065 <77864718+tel0065@users.noreply.github.com>
2022-12-06 13:05:20 -0600

    Update main.py

--- 2022/Day 6/Part 1/main.py
+++ 2022/Day 6/Part 1/main.py
@@ -1,20 +1,18 @@
+def findMarker(line, block_size): 
+    for c in range(len(line) - block_size + 1):
+        block = set(line[c:c + block_size])
+        if len(block) == block_size:
+            return block_size + c
+    return None
+
+
 def main():
     with open('input.txt') as file:
         lines = file.readlines()
     
     for line in lines:
         print(f'The first start-of-packet marker is complete after {findMarker(line, 4)} characters')
-    
-    for line in lines:
-        print(f'The first start-of-packet marker is complete after {findMarker(line, 14)} characters')
-        
-        
-def findMarker(line, block_size): 
-    for c in range(len(line) - block_size + 1):
-        block = set(line[c:c + block_size])
-        if len(block) == block_size:
-            return block_size + c
-    return None      
+     
 
 if __name__ == "__main__":
     main()
